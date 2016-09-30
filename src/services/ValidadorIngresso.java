5b90092e2080312c239b867cf81dcc8579c08f0d
José Flôres <arokhyn@gmail.com>
2016-09-30 00:46:18 -0300

    Adicionando a classe validador do ingresso

--- src/services/ValidadorIngresso.java
+++ src/services/ValidadorIngresso.java
@@ -1,5 +1,16 @@
 package services;
 
+import Model.Ingresso;
+
 public class ValidadorIngresso {
 
+	public void ValidarIngresso(Ingresso ingresso) throws Exception{
+		validarValorIngresso(ingresso);
+		
+	}
+	
+	public void validarValorIngresso(Ingresso ingresso){
+		
+		
+	}
 }
