5e7e5a1f37525fb4b8031590e172c286bd93a9a2
José Flôres <arokhyn@gmail.com>
2016-09-30 00:58:42 -0300

    Alterando o enumerator para servir como parametro de incializacao da classe ingresso

--- src/Enums/CategoriaIngresso.java
+++ src/Enums/CategoriaIngresso.java
@@ -1,5 +1,21 @@
 package Enums;
 
 public enum CategoriaIngresso {
-	VIP, BACKSTAGE, PLATEIA_VIP, PLATEIA
+	VIP("VIP", 1000), BACKSTAGE("BACKSTAGE", 800), PLATEIA_VIP("PLATEIA_VIP", 500), PLATEIA("PLATEIA", 300);
+
+	private final String type; // in kilograms
+	private final int preco; // in meters
+
+	CategoriaIngresso(String type, int preco) {
+		this.type = type;
+		this.preco = preco;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public int getPreco() {
+		return this.preco;
+	}
 }
