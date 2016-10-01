7d6b42e613325e3343d7105625d589e5b4b28221
JOSE PORTUGAL FARINA FLORES <ALU201631416@uniritter.local>
2016-09-30 22:10:46 -0300

    Adicionando o arquivo de configuracao do Travis

---
+++ src/model/ValidacaoIngressoException.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ValidacaoIngressoException  extends RuntimeException{
+	
+	public ValidacaoIngressoException(String mensagemErro) {
+		// TODO Auto-generated constructor stub
+		super(mensagemErro);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
